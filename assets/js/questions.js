a7b6293705698bea61b105255f5182485c3c9dc0
Shawn Lapuz <lapuzshawn@gmail.com>
2023-02-09 03:48:11 -0800

    fixed questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,19 @@
+//list of Questions
+var questionsArray = [
+    { question: 'what is 1+1?',
+      choicesArray: ['1','2', '3', '4'],
+      answer: '2', 
+    },
+    { question: 'what is 2+1?',
+    choicesArray: ['1','x', '3', '4'],
+    answer: '3', 
+    },
+    { question: 'what is 3+1?',
+      choicesArray: ['1','2', '3', '4'],
+      answer: '4',  
+    }
+  
+  ]
+  
+
+  // 
